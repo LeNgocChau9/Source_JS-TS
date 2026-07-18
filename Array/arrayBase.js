788426e0922a77cc75d41b184969c29e0bb2525c
LeNgocChau <LeNgocChau@gmail.gmailcom>
2026-07-18 21:26:59 +0700

    method filter

--- Array/arrayBase.js
+++ Array/arrayBase.js
@@ -40,3 +40,27 @@ console.log(scorev2);
     cách code rút gọn arrow function khi thân hàm chỉ có lệnh return
     const scorev2 = score.map((element,index) => element*2)
 */
+// hàm filter 
+const age = [19,2,21,23,25,6,3];
+// tìm 1 những người có số tuổi trên 18 tuổi
+/*
+    cơ chế hoạt động của hàm filter là kiểu hỏi cái này cần không thì giữ lại
+    ví dụ array = [chuối, cam , táo]
+    sẽ sẽ duyệt qua từng cái xong hỏi nếu điều kiện return là true thì giữ false thì loại bỏ
+    nó clone ra mảng mới không thay đổi mảng cũ trước đó
+*/
+console.log(age);
+
+const agev2 = age.filter((value) => value >18);
+// cơ chế nó là duyệt tuyến tín hết tất cả phần tử của mảng
+/*
+    ý tưởng  viết truyền thống của nó là
+    const ageclone = [];
+    for(int i = 0; i < age.length(), i++){
+        if(age[i]>18) ageclone.push(age[i]);
+    } 
+
+*/
+// cách code ngắn gọn khi không có gì trong thân arrow function 
+
+console.log(agev2);
