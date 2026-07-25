cfbae3d8717526e0ed48eb056962696e569a3929
LeNgocChau <LeNgocChau@gmail.gmailcom>
2026-07-25 11:01:50 +0700

    lab4 login

--- Event/script.js
+++ Event/script.js
@@ -2,8 +2,24 @@ const hanldClick = () => {
     console.log('click');
 }
 
-const text = document.getElementById('btn');
+const texth1 = document.getElementById('h1');
 
 const changeText = () => {
-    text.innerHTML = "Slownow";
+    texth1.innerHTML = "Slownow";
 }
+
+
+// cách viết js không nhúng dô html
+
+const changeColor = document.getElementById("changeColor");
+
+changeColor.addEventListener("click", () => {
+    texth1.style.color = "yellow";
+})
+
+
+const changeBack = document.getElementById("changeBack");
+
+changeBack.addEventListener("click", () => {
+    texth1.style.color = "black";
+})
