1bf919a73b135100bc98b8ab5a3bb089fb8ceae5
LeNgocChau <LeNgocChau@gmail.gmailcom>
2026-07-31 09:24:47 +0700

    insert data table dynamic

--- Mini_Project/script.js
+++ Mini_Project/script.js
@@ -2,5 +2,20 @@ const fetchProducts = async() => {
     const res = await fetch("https://dummyjson.com/products");
     const data = await res.json();
     console.log(data);
+
+    const tbody = document.querySelector("#Products tbody");
+
+    if (data && data.products && data.products.length) {
+        data.products.forEach((product) => {
+            tbody.innerHTML += `
+            <tr>
+                <td>${product.id}</td>
+                <td>${product.title}</td>
+                <td>${product.description}</td>
+                <td><img src="${product.thumbnail}" alt="${product.title}"></td>
+            </tr>
+            `
+        })
+    }
 }
 fetchProducts();
